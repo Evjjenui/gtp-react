5ff09d99f76f0e4419799faf792fd5f393a8fdd7
evjeniy <metall.92@inbox.ru>
2021-02-03 17:14:45 +0200

    upd krestiki

--- src/components/parts/TicTacToe.js
+++ src/components/parts/TicTacToe.js
@@ -7,6 +7,7 @@ class TicTacToe extends React.Component {
     super(props);
     this.updateItem = this.updateItem.bind(this);
     this.resetGame = this.resetGame.bind(this);
+    this.fieldStateCheck = this.fieldStateCheck.bind(this);
     this.state = {
       items: ['', '', '', '', '', '', '', '', ''],
       squareSign: 'x'
@@ -20,12 +21,46 @@ class TicTacToe extends React.Component {
     })
   }
 
-  updateItem(index) {
-    this.setState(krestiki(this.state, index));
+  fieldStateCheck() {
+
+    if (
+      this.state.items[0] === 'x' && this.state.items[1] === 'x' && this.state.items[2] === 'x' ||
+      this.state.items[3] === 'x' && this.state.items[4] === 'x' && this.state.items[5] === 'x' ||
+      this.state.items[6] === 'x' && this.state.items[7] === 'x' && this.state.items[8] === 'x' ||
+  
+      this.state.items[0] === 'x' && this.state.items[3] === 'x' && this.state.items[6] === 'x' ||
+      this.state.items[1] === 'x' && this.state.items[4] === 'x' && this.state.items[7] === 'x' ||
+      this.state.items[2] === 'x' && this.state.items[5] === 'x' && this.state.items[8] === 'x' ||
+  
+      this.state.items[0] === 'x' && this.state.items[4] === 'x' && this.state.items[8] === 'x' ||
+      this.state.items[2] === 'x' && this.state.items[4] === 'x' && this.state.items[6] === 'x'
+     ) {
+      return alert(`"X" WIN !!!`);
+     } else if (
+      this.state.items[0] === 'o' && this.state.items[1] === 'o' && this.state.items[2] === 'o' ||
+      this.state.items[3] === 'o' && this.state.items[4] === 'o' && this.state.items[5] === 'o' ||
+      this.state.items[6] === 'o' && this.state.items[7] === 'o' && this.state.items[8] === 'o' ||
+  
+      this.state.items[0] === 'o' && this.state.items[3] === 'o' && this.state.items[6] === 'o' ||
+      this.state.items[1] === 'o' && this.state.items[4] === 'o' && this.state.items[7] === 'o' ||
+      this.state.items[2] === 'o' && this.state.items[5] === 'o' && this.state.items[8] === 'o' ||
+  
+      this.state.items[0] === 'o' && this.state.items[4] === 'o' && this.state.items[8] === 'o' ||
+      this.state.items[2] === 'o' && this.state.items[4] === 'o' && this.state.items[6] === 'o'
+     ) {
+      return alert(`"O" WIN !!!`);
+     } else {
+      return;
+    }
+  }
+
+  updateItem(nextSign, index) {
+    this.setState(krestiki(this.state, index, nextSign), this.fieldStateCheck);
   };
 
   render () {
     const items = this.state.items;
+    let nextSign = 'o';
     
     return (
 
@@ -35,7 +70,7 @@ class TicTacToe extends React.Component {
             <TicItem
               key={index}
               value={item}
-              clickHandler={ () => this.updateItem(index)}/>
+              clickHandler={ () => this.updateItem(nextSign, index)}/>
           ))}
         </div>
         <button
--- src/components/parts/krestiki/krestiki.js
+++ src/components/parts/krestiki/krestiki.js
@@ -1,34 +1,11 @@
 export default function krestiki(stateObj, index) {
   let itemsCopy = [...stateObj.items];
   let selectedItem = itemsCopy[index];
-
-  if (
-    stateObj.items[0] === 'x' && stateObj.items[1] === 'x' && stateObj.items[2] === 'x' ||
-    stateObj.items[3] === 'x' && stateObj.items[4] === 'x' && stateObj.items[5] === 'x' ||
-    stateObj.items[6] === 'x' && stateObj.items[7] === 'x' && stateObj.items[8] === 'x' ||
-
-    stateObj.items[0] === 'x' && stateObj.items[3] === 'x' && stateObj.items[6] === 'x' ||
-    stateObj.items[1] === 'x' && stateObj.items[4] === 'x' && stateObj.items[7] === 'x' ||
-    stateObj.items[2] === 'x' && stateObj.items[5] === 'x' && stateObj.items[8] === 'x' ||
-
-    stateObj.items[0] === 'x' && stateObj.items[4] === 'x' && stateObj.items[8] === 'x' ||
-    stateObj.items[2] === 'x' && stateObj.items[4] === 'x' && stateObj.items[6] === 'x' ||
-
-    stateObj.items[0] === 'o' && stateObj.items[1] === 'o' && stateObj.items[2] === 'o' ||
-    stateObj.items[3] === 'o' && stateObj.items[4] === 'o' && stateObj.items[5] === 'o' ||
-    stateObj.items[6] === 'o' && stateObj.items[7] === 'o' && stateObj.items[8] === 'o' ||
-
-    stateObj.items[0] === 'o' && stateObj.items[3] === 'o' && stateObj.items[6] === 'o' ||
-    stateObj.items[1] === 'o' && stateObj.items[4] === 'o' && stateObj.items[7] === 'o' ||
-    stateObj.items[2] === 'o' && stateObj.items[5] === 'o' && stateObj.items[8] === 'o' ||
-
-    stateObj.items[0] === 'o' && stateObj.items[4] === 'o' && stateObj.items[8] === 'o' ||
-    stateObj.items[2] === 'o' && stateObj.items[4] === 'o' && stateObj.items[6] === 'o'
-  ) {
-    return alert(`"${stateObj.squareSign}" WIN !!!`);
-  }
-
+  console.log('HERE');
+  console.log(itemsCopy, selectedItem, stateObj.squareSign);
+  
   if (!selectedItem) {
+    console.log('reinit array');
     selectedItem = stateObj.squareSign;
     itemsCopy[index] = selectedItem;
   
