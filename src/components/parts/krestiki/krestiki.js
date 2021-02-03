cf2456d74fda237140f6b10ae6a6a3822ef86a49
evjeniy <metall.92@inbox.ru>
2021-02-03 17:15:34 +0200

    upd krestiki

--- src/components/parts/krestiki/krestiki.js
+++ src/components/parts/krestiki/krestiki.js
@@ -1,11 +1,8 @@
 export default function krestiki(stateObj, index) {
   let itemsCopy = [...stateObj.items];
   let selectedItem = itemsCopy[index];
-  console.log('HERE');
-  console.log(itemsCopy, selectedItem, stateObj.squareSign);
   
   if (!selectedItem) {
-    console.log('reinit array');
     selectedItem = stateObj.squareSign;
     itemsCopy[index] = selectedItem;
   
