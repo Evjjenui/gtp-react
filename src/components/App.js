88f1c47bd309ebf104010bf8d8612eda40aec700
evjeniy <metall.92@inbox.ru>
2021-02-04 18:29:35 +0200

    add routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,53 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Calculator from "./parts/Calculator";
 import TicTacToe from "./parts/TicTacToe";
+import Markdown from "./parts/Markdown";
+import Timer from "./parts/Timer";
+import Todo from "./parts/Todo";
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        {/* <Calculator /> */}
-        <TicTacToe />
+        <Router>
+          <ul className="nav">
+            <li>
+              <Link to='/calculator'>Calculator</Link>
+            </li>
+            <li>
+              <Link to='/markdown'>Markdown</Link>
+            </li>
+            <li>
+              <Link to='/krestiki'>Krestiki</Link>
+            </li>
+            <li>
+              <Link to='/timer'>Timer</Link>
+            </li>
+            <li>
+              <Link to='/todo'>Todo</Link>
+            </li>
+          </ul>
+
+          <Switch>
+            <Route exact path='/calculator'>
+              <Calculator />
+            </Route>
+            <Route path='/markdown'>
+              <Markdown />
+            </Route>
+            <Route path='/krestiki'>
+              <TicTacToe />
+            </Route>
+            <Route path='/timer'>
+              <Timer />
+            </Route>
+            <Route path='/todo'>
+              <Todo />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     )
   }
