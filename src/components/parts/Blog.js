853204ef40ac356c28766573cf0181f0d3219bc6
evjeniy <metall.92@inbox.ru>
2021-04-28 22:37:58 +0300

    add blog

--- src/components/parts/Blog.js
+++ src/components/parts/Blog.js
@@ -1,25 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
-
+import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch} from "react-router-dom";
+import BlogCreate from "./BlogCreate";
 import BlogList from "./particles/BlogList"
 
 const Blog = () => {
+  let { path, url } = useRouteMatch();
 
   return ( 
 
     <div>
-      <div className="blog-head">
-        <h1>Blog main page</h1>
-
-      </div>
       <Router>
+        <div className="blog-head">
+          <h1>Blog</h1>
+          <div className="blog-links">
+            <Link to={`${url}`}>Blog</Link>
+            <Link to={`${url}/blog-create`}>Add New</Link>
+          </div>
+        </div>
         <Switch >
-          <Route exact path="/blog">
+          <Route exact path={path}>
             <BlogList />
           </Route>
-          {/* <Route>
-            <BlogCreate/>
-          </Route> */}
+          <Route path={`${path}/blog-create`}>
+            <BlogCreate />
+          </Route>
         </Switch>
       </Router>
     </div>
---
+++ src/components/parts/BlogCreate.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const BlogCreate = () => {
+  return (
+    <div className="blog-form">
+      <h2>Create New Blog</h2>
+      <form>
+
+      </form>
+    </div>
+  );
+}
+ 
+export default BlogCreate;
